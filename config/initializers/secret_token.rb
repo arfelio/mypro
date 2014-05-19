3960911e8d86a311576021d1996b0939e35dae4e
arfelio <arfelio1980@gmail.com>
2014-05-19 19:58:48 +0300

    add blog to stage

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = 'ca4fac6a0ecaadf96c9c733e48411b88c96c28db17ae42f31c39355680a42e72b92aa9d5dbdc09dd44df48d2467e77de98a36a5da10ab11e03e8d46452e1195a'
